1153df5dc288426ff4c264d0b99d58cb52be5161
sakoh <sakohartounian@yahoo.com>
2016-04-26 20:00:07 -0700

    directive for navigation now shared between registration and main menu

--- app/directives/navigation.js
+++ app/directives/navigation.js
@@ -1,22 +1,13 @@
 angular.module('govApp.directives.navigation',[]).directive('navigation', function() {
   return {
     scope: {
-			links: "@"
+			links: "="
 		},
+    transclude: true,
     restrict: 'EA',
     replace: true,
-    templateUrl: "partials/navigation.html",
-    link: function(scope, element, attrs){
-      scope.links = [{
-        id: 'home',
-        title: 'Home'
-      }, {
-        id: 'about',
-        title: 'About'
-      }, {
-        id: 'registration',
-        title: 'Registration'
-      }];
+    templateUrl: function(elem, attrs) {
+      return attrs.templateUrl || "partials/navigation.html";
     }
 	}
 });
